15ed90ae319993f956cdae354c49f4bbfd8058bc
stubee84 <stuartbrannon@gmail.com>
2021-05-05 06:50:02 -0400

    removed stdout print from test_general_consumer

--- livechatapp/tests/async_tests.py
+++ livechatapp/tests/async_tests.py
@@ -5,7 +5,6 @@ import json, pytest
 @pytest.mark.asyncio
 async def test_general_consumer():
     communicator = WebsocketCommunicator(GeneralChatConsumer, path="/chat/general")
-    print("test message")
     connected, subprotocol = await communicator.connect()
     assert connected
 
@@ -13,7 +12,6 @@ async def test_general_consumer():
     await communicator.send_to(text_data=json.dumps(send_msg))
     rcv_msg = await communicator.receive_output()
 
-    print(rcv_msg)
     assert send_msg == json.loads(rcv_msg['text'])
 
     await communicator.disconnect()
